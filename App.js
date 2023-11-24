b3909d49b32d69f921df5b62e5577f4530b048f7
Arjesh <arjesh.khadka2@gmail.com>
2023-11-25 10:22:31 +1100

    feat: icon setup on navigation

--- App.js
+++ App.js
@@ -5,16 +5,28 @@ import { NavigationContainer } from '@react-navigation/native';
 import ManageExpenses from './screens/ManageExpenses';
 import RecentExpenses from './screens/RecentExpenses';
 import AllExpenses from './screens/AllExpenses';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import { GlobalStyles } from './constants/styles';
 
 const Stack = createNativeStackNavigator();
 const ButtomTab = createMaterialBottomTabNavigator();
 
 function ExpensesOverview() {
-  return(
-    <ButtomTab.Navigator>
-      <ButtomTab.Screen name='RecentExpenses' component={RecentExpenses}/>
-      <ButtomTab.Screen name='AllExpenses' component={AllExpenses}/>
-    </ButtomTab.Navigator>
+  return (
+    <ButtomTab.Navigator barStyle={{ backgroundColor: GlobalStyles.colors.primary400 }} activeColor={GlobalStyles.colors.white} inactiveColor={GlobalStyles.colors.accent500} >
+      <ButtomTab.Screen name='RecentExpenses' component={RecentExpenses} options={{
+        tabBarLabel: 'Recent Expenses',
+        tabBarIcon: ({ color, focused }) => <FontAwesome name="clock-o" color={focused ? 'black' : color} size={26} />
+      }} />
+      <ButtomTab.Screen name='AllExpenses' component={AllExpenses} options={{
+        tabBarLabel: 'All Expenses',
+        tabBarIcon: ({ color, focused }) => <FontAwesome name="list" color={focused ? 'black' : color} size={26} />,
+        tabBarColor: 'red'
+
+
+
+      }} />
+    </ ButtomTab.Navigator>
   )
 }
 
@@ -24,8 +36,8 @@ export default function App() {
       <StatusBar style="auto" />
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name='ExpensesOverview' component={ExpensesOverview} options={{headerShown: false}}/>
-          <Stack.Screen name='ManageExpense' component={ManageExpenses}/>
+          <Stack.Screen name='ExpensesOverview' component={ExpensesOverview} options={{ headerShown: false }} />
+          <Stack.Screen name='ManageExpense' component={ManageExpenses} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
--- constants/styles.js
+++ constants/styles.js
@@ -1,16 +1,17 @@
 export const GlobalStyles = {
-    colors:{
-      primary50: '#f4ebff',
-      primary100: '#d8bfff',
-      primary200: '#b187f5',
-      primary400: '#5f24d9',
-      primary500: '#4505c7',
-      primary700: '#350b91',
-      primary800: '#280647',
-      accent500: '#f7bc0c',
-      error50: '#ffd7e6',
-      error500: '#9b095c',
-      gray500: '#63557c',
-      gray700: '#352842',
-    }
+  colors: {
+    primary50: '#f4ebff',
+    primary100: '#d8bfff',
+    primary200: '#b187f5',
+    primary400: '#5f24d9',
+    primary500: '#4505c7',
+    primary700: '#350b91',
+    primary800: '#280647',
+    accent500: '#f7bc0c',
+    error50: '#ffd7e6',
+    error500: '#9b095c',
+    gray500: '#63557c',
+    gray700: '#352842',
+    white: '#ffff'
+  }
 }
