dd60f8acf832003849a26c541833a37f46f5eaa4
Arjesh <arjesh.khadka2@gmail.com>
2023-11-25 11:28:15 +1100

    feat: mock data added

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView, StyleSheet } from 'react-native';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
@@ -7,32 +8,31 @@ import RecentExpenses from './screens/RecentExpenses';
 import AllExpenses from './screens/AllExpenses';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import { GlobalStyles } from './constants/styles';
+import Dashboard from './screens/Dashboard';
 
 const Stack = createNativeStackNavigator();
 const ButtomTab = createMaterialBottomTabNavigator();
 
 function ExpensesOverview() {
   return (
-    <ButtomTab.Navigator barStyle={{ backgroundColor: GlobalStyles.colors.primary400 }} activeColor={GlobalStyles.colors.white} inactiveColor={GlobalStyles.colors.accent500} >
-      <ButtomTab.Screen name='RecentExpenses' component={RecentExpenses} options={{
-        tabBarLabel: 'Recent Expenses',
-        tabBarIcon: ({ color, focused }) => <FontAwesome name="clock-o" color={focused ? 'black' : color} size={26} />
+    <ButtomTab.Navigator barStyle={{ backgroundColor: GlobalStyles.colors.primary400, padding: 2 }} theme={'red'} activeColor={GlobalStyles.colors.white} inactiveColor={GlobalStyles.colors.accent500} >
+      <ButtomTab.Screen name='Dashboard' component={Dashboard} options={{
+        tabBarLabel: 'Dashboard',
+        tabBarIcon: ({ color, focused }) => <FontAwesome name="dashboard" color={focused ? 'black' : color} size={26} />,
       }} />
       <ButtomTab.Screen name='AllExpenses' component={AllExpenses} options={{
         tabBarLabel: 'All Expenses',
         tabBarIcon: ({ color, focused }) => <FontAwesome name="list" color={focused ? 'black' : color} size={26} />,
         tabBarColor: 'red'
-
-
-
       }} />
+
     </ ButtomTab.Navigator>
   )
 }
 
 export default function App() {
   return (
-    <>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
       <NavigationContainer>
         <Stack.Navigator>
@@ -40,8 +40,14 @@ export default function App() {
           <Stack.Screen name='ManageExpense' component={ManageExpenses} />
         </Stack.Navigator>
       </NavigationContainer>
-    </>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+})
 
 
---
+++ components/TransactionList.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const TransactionList = ({ items }) => {
+    return (
+        <View>
+            <View>
+
+            </View>
+
+
+        </View>
+
+
+
+    )
+}
+
+export default TransactionList
