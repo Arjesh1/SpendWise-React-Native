9864f0d8e5a2c3e4845a057f97dde36fcdcd038a
Arjesh <arjesh.khadka2@gmail.com>
2023-12-30 22:56:19 +1100

    style: login screen styling. # 5m

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,9 +1,14 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
+import { GlobalStyles } from '../constants/styles'
 
 const LoginScreen = () => {
   return (
-    <View>
+    <View style={styles.loginWrapper}>
+        <View style={styles.loginTopBanner}>
+            <Text> hello</Text>
+
+        </View>
         <Text>Login Screen</Text>
       
     </View>
@@ -11,4 +16,16 @@ const LoginScreen = () => {
 }
 
 export default LoginScreen
+const styles = StyleSheet.create({
+    loginWrapper:{
+        flex: 1,
+    },
+    loginTopBanner:{
+        backgroundColor: GlobalStyles.colors.primary600,
+        height:'22%',
+        borderBottomLeftRadius: 99,
+        borderBottomRightRadius: 99,
+    }
+
+})
 
