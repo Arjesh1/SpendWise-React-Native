037635fa3fef88fec9738acaed078800c9d1bb5d
Arjesh <arjesh.khadka2@gmail.com>
2023-12-30 22:42:25 +1100

    feat: native stack navigation setup for login screen

--- App.js
+++ App.js
@@ -9,32 +9,47 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Provider } from "react-redux";
 import store from './reduxStore/store';
 import Profile from './screens/Profile';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './screens/LoginScreen';
 
 const ButtomTab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator()
 
 export default function App() {
+
+  function ButtomTabNavigation(){
+    return(
+      <ButtomTab.Navigator screenOptions={{
+        tabBarStyle: { backgroundColor: '#020617', justifyContent: 'flex-end', alignItems: 'flex-end' },
+        headerShown: false,
+        tabBarActiveTintColor: GlobalStyles.colors.primary700,
+      }}  >
+        <ButtomTab.Screen name='Dashboard' component={Dashboard} options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ focused }) => <FontAwesome name="home" color={focused ? GlobalStyles.colors.primary700 : 'white'} size={26} />,
+        }} />
+        <ButtomTab.Screen name='Transactions' component={AllExpenses} options={{
+          tabBarLabel: 'Transactions',
+          tabBarIcon: ({ focused }) => <FontAwesome name="list" color={focused ? GlobalStyles.colors.primary700 : 'white'} size={26} />,
+        }} />
+        <ButtomTab.Screen name='Profile' component={Profile} options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: ({ focused }) => <FontAwesome name="user" color={focused ? GlobalStyles.colors.primary700 : 'white'} size={26} />,
+        }} />
+      </ ButtomTab.Navigator>
+    )
+  }
+
+  
   return (
     <Provider store={store}>
       <StatusBar style="light" />
       <NavigationContainer>
-        <ButtomTab.Navigator screenOptions={{
-          tabBarStyle: { backgroundColor: '#020617', justifyContent:'flex-end', alignItems: 'flex-end' },
-          headerShown: false,
-          tabBarActiveTintColor: GlobalStyles.colors.primary700,
-        }}  >
-          <ButtomTab.Screen name='Dashboard' component={Dashboard} options={{
-            tabBarLabel: 'Home',
-            tabBarIcon: ({ focused }) => <FontAwesome name="home" color={focused ? GlobalStyles.colors.primary700 : 'white'} size={26} />,
-          }} />
-          <ButtomTab.Screen name='Transactions' component={AllExpenses} options={{
-            tabBarLabel: 'Transactions',
-            tabBarIcon: ({ focused }) => <FontAwesome name="list" color={focused ? GlobalStyles.colors.primary700 : 'white'} size={26} />,
-          }} />
-          <ButtomTab.Screen name='Profile' component={Profile} options={{
-            tabBarLabel: 'Profile',
-            tabBarIcon: ({ focused }) => <FontAwesome name="user" color={focused ? GlobalStyles.colors.primary700 : 'white'} size={26} />,
-          }} />
-        </ ButtomTab.Navigator>
+        <Stack.Navigator screenOptions={{ headerShown: false}} initialRouteName='Login'>
+          <Stack.Screen name='Home' component={ButtomTabNavigation}/>
+          <Stack.Screen name='Login' component={LoginScreen} />
+        </Stack.Navigator>
+        
       </NavigationContainer>
     </Provider>
   );
